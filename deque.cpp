8ff032ff7e9c2272ea1ccb56457706978920def4
Dylan Daniels <ddanie31@ut.utm.edu>
2024-12-08 19:06:36 -0600

    added functions working on implemntations rn

--- deque.cpp
+++ deque.cpp
@@ -1,1 +1,18 @@
 //deque cpp file
+#include "deque.h"
+#include <iostream>
+
+using namespace std;
+
+deque::deque(){}
+deque::~deque(){}
+void deque::resize(){}
+void deque::pop_front(){}
+void deque::pop_back(){}
+void deque::push_front(){}
+void deque::push_back(){}
+int deque::front(){}
+int deque::back(){}
+int deque::size(){}
+bool deque::empty(){}
+// [] opperator
--- deque.h
+++ deque.h
@@ -1,1 +1,31 @@
 //deque.h file
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <iostream>
+
+using namespace std;
+
+class deque{
+ private:
+  int *front_ptr, *back_ptr, rows;
+  static const int block_size = 4;
+  int ** blockmap;
+  void resize();
+ public:
+  deque();
+  ~deque();
+  void push_front();
+  void pop_front();
+  void push_back();
+  void pop_back();
+  int front();
+  int back();
+  bool empty();
+  int size();
+  // [] operator
+  
+}
+
+
+#endif //DEQUE_H
